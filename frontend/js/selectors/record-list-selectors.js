7f35f05934d947a175cb83e1c03d1ade9a685eb0
Pasi Tuominen <pasi.tuominen@gmail.com>
2016-10-21 11:09:42 +0300

    Adds selector for parse errors

--- frontend/js/selectors/record-list-selectors.js
+++ frontend/js/selectors/record-list-selectors.js
@@ -3,7 +3,7 @@ import { createSelector } from 'reselect';
 const recordList = state => state.getIn(['jobconfig', 'rawRecordIdRows']);
 
 export const validRecordList = createSelector([recordList], (recordList) => {
-  return recordList.filter(row => row !== '');
+  return recordList.filter(row => row !== undefined);
 });
 
 export const validRecordCount = createSelector(
@@ -12,3 +12,12 @@ export const validRecordCount = createSelector(
     return validRecordList.length;
   }
 );
+
+export const recordParseErrors = createSelector([recordList], (recordList) => {
+  return recordList.reduce((acc, row, i) => {
+    if (row instanceof Error) {
+      acc.push({row: i, error: row});
+    }
+    return acc;
+  }, []);
+});
