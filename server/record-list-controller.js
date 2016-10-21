e89f6ec05d370fd7680d43c80f5dee05d6f151f5
Pasi Tuominen <pasi.tuominen@gmail.com>
2016-10-21 11:08:08 +0300

    Validate format

--- server/record-list-controller.js
+++ server/record-list-controller.js
@@ -7,6 +7,7 @@ import HttpStatus from 'http-status-codes';
 import { connect, startJob } from './record-list-service';
 import { requireSession, readSessionMiddleware } from './session-controller';
 import cookieParser from 'cookie-parser';
+import { validate } from './common/input-parser';
 
 // Connect to AMQP host
 connect();
@@ -23,6 +24,9 @@ recordListController.post('/', cors(corsOptions), requireSession, requireBodyPar
   const { sessionToken } = req.cookies;
 
   try {
+    if (!records.every(validate)) {
+      return res.sendStatus(HttpStatus.BAD_REQUEST);
+    }
     startJob(records, lowTag, sessionToken);
   } catch(error) {
     logger.log('error', 'Unable to start job', error);
