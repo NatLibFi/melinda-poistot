335950af08be2047685a9c9e9e9f2114f511b80e
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2017-10-24 11:55:08 +0300

    Change license & more
    1) Change license to AGPL-3.0+
    2) Update copyright
    3) Update melinda-ui-commons
    4) Add postinstall script

--- frontend/js/reducers/record-list-form-reducer.js
+++ frontend/js/reducers/record-list-form-reducer.js
@@ -1,4 +1,30 @@
-import { Map } from 'immutable';
+/**
+*
+* @licstart  The following is the entire license notice for the JavaScript code in this file.
+*
+* UI for removing references to local databases from Melinda
+*
+* Copyright (C) 2016-2017 University Of Helsinki (The National Library Of Finland)
+*
+* This file is part of melinda-local-ref-removal-ui
+*
+* melinda-local-ref-removal-ui program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* melinda-local-ref-removal-ui is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* @licend  The above is the entire license notice
+* for the JavaScript code in this file.
+*
+*/import { Map } from 'immutable';
 import {SET_SELECTED_LOW_TAG, SET_RECORD_ID_LIST, SUBMIT_JOB_START, SUBMIT_JOB_SUCCESS, SUBMIT_JOB_FAIL, SET_DELETE_OPTION, SET_REPLICATE_OPTION} from '../constants/action-type-constants';
 import { RESET_WORKSPACE } from '../constants/action-type-constants';
 
