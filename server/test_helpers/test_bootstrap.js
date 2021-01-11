d51509e0a366b7dcd540503846aad3b8cc2049a3
Minttu Hurme <ammsalme@users.noreply.github.com>
2021-01-11 09:17:32 +0200

    Fix drone and docker (#79)
    * Fix drone and docker

--- server/test_helpers/test_bootstrap.js
+++ server/test_helpers/test_bootstrap.js
@@ -25,7 +25,7 @@
 * for the JavaScript code in this file.
 *
 */process.env.ALEPH_URL='test-url';
-process.env.ALEPH_USER_LIBRARY='test-lib';
+process.env.ALEPH_USER_LIBRARY='test-lib'; //ignore:node_username
 process.env.DUPLICATE_DB_URL='test-duplicate-db-url';
 process.env.AMQP_HOST='test-amqp-host';
 process.env.SMTP_CONNECTION_URL='smtp://test:test@localhost:2525';
